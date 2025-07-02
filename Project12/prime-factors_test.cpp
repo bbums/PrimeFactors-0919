2bbd801616bd0d0fccd2d235965501eaa2cbfdb0
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 10:46:19 +0900

    [feature] p56

--- Project12/prime-factors_test.cpp
+++ Project12/prime-factors_test.cpp
@@ -28,3 +28,8 @@ TEST_F(PrimeFactorsFixture, Of4) {
 	std::vector<int> expected = {2, 2};
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST_F(PrimeFactorsFixture, Of5) {
+	std::vector<int> expected = {2, 3};
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
