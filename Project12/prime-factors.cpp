a9e884933949fb5f6d6d4995f8dcc00f382d9b96
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 10:12:03 +0900

    [feature] p45

--- Project12/prime-factors.cpp
+++ Project12/prime-factors.cpp
@@ -2,8 +2,10 @@
 
 class PrimeFactors {
 public:
-	std::vector<int> of(int v) {
-		std::vector<int> res;
+	std::vector<int> of(int number) {
+		std::vector<int> res = {};
+		if (number == 2)
+			res.push_back(2);
 		return res;
 	}
 private:
--- Project12/prime-factors_test.cpp
+++ Project12/prime-factors_test.cpp
@@ -9,3 +9,9 @@ TEST(PrimeFactors, Of1) {
 	std::vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	std::vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
