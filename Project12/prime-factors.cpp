75a5ab278ea36b17c2fd0aaf6404f641abfe7199
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 10:58:42 +0900

    [refactor2] p65

--- Project12/prime-factors.cpp
+++ Project12/prime-factors.cpp
@@ -5,14 +5,9 @@ public:
 	std::vector<int> of(int number) {
 		std::vector<int> res = {};
 
-		if (number >= 2) {
-			for (int devisor = 2; devisor <= 3 && number != 1; devisor++) {
-				tryDevideBy(devisor, number, res);
-			}
+		for (int devisor = 2; number > 1; devisor++) {
+			tryDevideBy(devisor, number, res);
 		}
-
-		if (number != 1)
-			res.push_back(number);
 
 		return res;
 	}
