ff33ea76de5bfb6288195251076dc366a4491cea
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 10:27:50 +0900

    [refactor] p51

--- Project12/prime-factors.cpp
+++ Project12/prime-factors.cpp
@@ -4,13 +4,14 @@ class PrimeFactors {
 public:
 	std::vector<int> of(int number) {
 		std::vector<int> res = {};
-		if (number == 1) return res;
-		else if (number == 4) {
-			res.push_back(2);
-			res.push_back(2);
+		if (number >= 2) {
+			while (number % 2 == 0) {
+				res.push_back(2);
+				number /= 2;
+			}
+			if(number != 1)
+				res.push_back(number);
 		}
-		else if (number >= 2) 
-			res.push_back(number);
 		return res;
 	}
 private:
